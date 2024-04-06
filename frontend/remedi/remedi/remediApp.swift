d11f45fc539ec47b939e3bcb31f2927499be531b
Rushil695 <rushil9rock@gmail.com>
2024-04-06 06:48:21 -0400

    first

---
+++ frontend/remedi/remedi/remediApp.swift
@@ -0,0 +1,17 @@
+//
+//  remediApp.swift
+//  remedi
+//
+//  Created by Rushil Madhu on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct remediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
